39366de44a58895b8151361a9fb93b6c3af1c389
FvonAhn <fredd24hate@gmail.com>
2024-12-09 11:36:33 +0100

    changed some stuff in the dialog when you want to add a new contact.

--- ContactList/Factory/ContactFactory.cs
+++ ContactList/Factory/ContactFactory.cs
@@ -5,12 +5,33 @@ public class ContactFactory
 {
     public void AddContactFactory()
     {
-        Console.WriteLine("Add new contact.");
-        Console.ReadKey();
-        Console.Write("First name: "); string addFirstName = Console.ReadLine()!;
-        Console.Write("Last name: "); string addLastName = Console.ReadLine()!;
-        Console.Write("Adress: "); string addAdress = Console.ReadLine()!;
-        Console.Write("Email: "); string addEmail = Console.ReadLine()!;
-        Console.Write("Phone number: "); string addPhone = Console.ReadLine()!;
+        bool addContactBool = true;
+        while (addContactBool) 
+        {
+            Console.Clear();
+            Console.WriteLine("Add new contact.");
+            Console.WriteLine($"{"",-5}Y / N");
+            string addContactAnswer = Console.ReadLine()!;
+
+            if (addContactAnswer.ToLower() == "y")
+            {
+                Console.Clear();
+                Console.Write("First name: "); string addFirstName = Console.ReadLine()!;
+                Console.Write("Last name: "); string addLastName = Console.ReadLine()!;
+                Console.Write("Adress: "); string addAdress = Console.ReadLine()!;
+                Console.Write("Email: "); string addEmail = Console.ReadLine()!;
+                Console.Write("Phone number: "); string addPhone = Console.ReadLine()!;
+                Console.Clear();
+                Console.WriteLine("New contact added.");
+                addContactBool = false;
+
+            }
+            if (addContactAnswer.ToLower() == "n")
+            {
+                Console.Clear();
+                Console.WriteLine("No new contacts added.");
+                addContactBool = false;
+            }
+        }
     }
 }
--- ContactList/Models/Contacts.cs
+++ ContactList/Models/Contacts.cs
@@ -4,24 +4,24 @@ public class Contacts
     public int ID { get; set; }
     public string FirstName { get; set; } = null!;
     public string LastName { get; set; } = null!;
-    public string Adress { get; set; } = null!;
+    public string Address { get; set; } = null!;
     public string Email { get; set; } = null!;
     public string Phone { get; set; } = null!;
 
-    protected Contacts(string firstName, string lastName, string adress, string email)
+    protected Contacts(string firstName, string lastName, string address, string email)
     {
         if (string.IsNullOrEmpty(firstName))
             throw new ArgumentException("First name field cannot be empty");
         if (string.IsNullOrEmpty(lastName))
             throw new ArgumentException("Last name field cannot be empty");
-        if (string.IsNullOrEmpty(adress))
+        if (string.IsNullOrEmpty(address))
             throw new ArgumentException("Adress field cannot be empty");
         if (string.IsNullOrEmpty(email))
             throw new ArgumentException("Email cannot be empty");
 
         FirstName = firstName;
         LastName = lastName;
-        Adress = adress;
+        Address = address;
         Email = email;
     }
 }
